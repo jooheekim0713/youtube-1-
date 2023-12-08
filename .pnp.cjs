ce26170587ccce06c57498e6f039300fc2d03d4d
jooheekim <jooheekim.dev@gmail.com>
2023-12-08 16:02:47 +0900

    Feat : 비디오카드 화면 구현

--- .pnp.cjs
+++ .pnp.cjs
@@ -40,6 +40,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["react-router-dom", "virtual:3fab3b154eadc58f7ac055ad744bc95f4a1f3d694428aae2c61e4e351c02a636eb34d217ac10dc9dbc114c79fdaf80a15cc20ffc43aea48ad94efff78372942f#npm:6.20.1"],\
             ["react-scripts", "virtual:3fab3b154eadc58f7ac055ad744bc95f4a1f3d694428aae2c61e4e351c02a636eb34d217ac10dc9dbc114c79fdaf80a15cc20ffc43aea48ad94efff78372942f#npm:5.0.1"],\
             ["tailwindcss", "npm:3.3.6"],\
+            ["timeago.js", "npm:4.0.2"],\
             ["web-vitals", "npm:2.1.4"]\
           ],\
           "linkType": "SOFT"\
@@ -17005,6 +17006,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["timeago.js", [\
+        ["npm:4.0.2", {\
+          "packageLocation": "./.yarn/cache/timeago.js-npm-4.0.2-e87d64f9a3-c785a06988.zip/node_modules/timeago.js/",\
+          "packageDependencies": [\
+            ["timeago.js", "npm:4.0.2"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["tmpl", [\
         ["npm:1.0.5", {\
           "packageLocation": "./.yarn/cache/tmpl-npm-1.0.5-d399ba37e2-cd922d9b85.zip/node_modules/tmpl/",\
@@ -18392,6 +18402,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["react-router-dom", "virtual:3fab3b154eadc58f7ac055ad744bc95f4a1f3d694428aae2c61e4e351c02a636eb34d217ac10dc9dbc114c79fdaf80a15cc20ffc43aea48ad94efff78372942f#npm:6.20.1"],\
             ["react-scripts", "virtual:3fab3b154eadc58f7ac055ad744bc95f4a1f3d694428aae2c61e4e351c02a636eb34d217ac10dc9dbc114c79fdaf80a15cc20ffc43aea48ad94efff78372942f#npm:5.0.1"],\
             ["tailwindcss", "npm:3.3.6"],\
+            ["timeago.js", "npm:4.0.2"],\
             ["web-vitals", "npm:2.1.4"]\
           ],\
           "linkType": "SOFT"\
